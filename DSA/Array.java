5d6f19e1c130d2670a3a8f605c3e02d23e76ce81
Abhay Kumar <abhayvishwakrma102@gmail.com>
2026-06-21 15:42:23 +0530

    Refactor Array class with search and largest number methods
    Added methods for linear search and finding the largest number in an array. Cleaned up commented-out code and improved the structure of the Array class.

--- DSA/Array.java
+++ DSA/Array.java
@@ -38,5 +38,50 @@ public class Array{
         }
         System.out.println();
     }
+    // LINEAR SEARCH 
+    /*public static int linearSearch(int numbers[],int key){
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main (String arg[]){
+        int numbers[] = {2,4,6,8,10,12,14,16};
+        int key = 14;
+
+        int index = linearSearch(numbers,key);
+        if(index==-1){
+            System.out.println("Key not found in the array");
+        }
+        else{
+            System.out.println("Key found at index :"+ index);
+        }
+    }*/
+
+
+    // LARGEST NUMBER
+    public static int getlargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for(int i=0; i<numbers.length; i++){
+            if(largest< numbers[i]){
+                largest = numbers[i];
+            }
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest number :"+ smallest);
+        return largest;
+    }
+
+    public static void main(String arg[]){
+        int numbers[] = {1,2,6,3,5};
+        System.out.println("Largest number :"+ getlargest(numbers));
+    }
+    
 
 }
