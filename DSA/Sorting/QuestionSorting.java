74751d98b78f43ddbe566cb175f5c806eda2252f
webdevelopment <abhayvishwakrma102@gmail.com>
2026-07-05 12:18:21 +0530

    bubble and selection sort

---
+++ DSA/Sorting/QuestionSorting.java
@@ -0,0 +1,38 @@
+public class QuestionSorting{
+    public static void BubbleSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+    public static void SelectionSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minPos=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minPos]>arr[j]){
+                    minPos= j;
+                }
+            int temp = arr[minPos];
+            arr[minPos] =arr[i];
+            arr[i]=temp;
+            }
+        }
+    }
+    public static void PrintArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[]={3,6,2,1,8,7,4,5,3,1};
+        SelectionSort(arr);
+        PrintArray(arr);
+    }
+}
