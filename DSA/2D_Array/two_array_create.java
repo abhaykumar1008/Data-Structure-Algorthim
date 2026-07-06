3f2ca44b74afae227f41b7ebce1e6503a30ab999
webdevelopment <abhayvishwakrma102@gmail.com>
2026-07-06 12:57:33 +0530

    two dimesnsion array

---
+++ DSA/2D_Array/two_array_create.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class two_array_create{
+    public static boolean Search(int matrix[][], int key){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==key){
+                    System.out.println("Key found at index: ("+i+","+j+")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+    public static void main(String arg[]){
+        int matrix[][] =new int[3][3];
+        int n=matrix.length;
+        int m=matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+        Search(matrix,5);     
+
+    }
+    
+}
