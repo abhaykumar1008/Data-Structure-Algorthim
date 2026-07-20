f2fb8412a496a86c5c9eecc2e0d7c2ea33774977
webdevelopment <abhayvishwakrma102@gmail.com>
2026-07-20 14:02:12 +0530

    String will be start

---
+++ DSA/String/Strings.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Strings{
+    public static void main (String args[]){
+        char arr[] = {'a','b','c','d','e'};
+        String str ="abcd";
+        String str2 = new String("xyz");
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.next(); // taking one words
+        name = sc.nextLine(); // taking whole line
+        System.out.println(name);
+    }
+}
