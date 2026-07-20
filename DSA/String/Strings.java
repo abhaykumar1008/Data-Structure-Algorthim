692dae832c376e1d29e8aaa48d5257cb94030c27
webdevelopment <abhayvishwakrma102@gmail.com>
2026-07-20 14:07:20 +0530

    concatination

--- DSA/String/Strings.java
+++ DSA/String/Strings.java
@@ -1,7 +1,6 @@
-import java.util.*;
 public class Strings{
     public static void main (String args[]){
-        char arr[] = {'a','b','c','d','e'};
+        /*char arr[] = {'a','b','c','d','e'};
         String str ="abcd";
         String str2 = new String("xyz");
 
@@ -10,5 +9,14 @@ public class Strings{
         name = sc.next(); // taking one words
         name = sc.nextLine(); // taking whole line
         System.out.println(name);
+
+        String fullName = "Abhay Kumar";
+        System.out.println(fullName.length());*/
+        
+        // concatenation of strings
+        String str1 = "Abhay";
+        String str2 = "Kumar";
+        String str3 = str1 + " "+ str2;
+        System.out.println(str3);
     }
 }
