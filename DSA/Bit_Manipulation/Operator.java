0a2a3313897830a20d5ceeba5e4a2f3eb3ea7659
webdevelopment <abhayvishwakrma102@gmail.com>
2026-07-24 12:36:00 +0530

    operator

---
+++ DSA/Bit_Manipulation/Operator.java
@@ -0,0 +1,13 @@
+public class Operator{
+    public static void main(String args[]){
+        System.out.println(5&6); // And Operator
+        System.out.println(5|6); // Or Operator
+        System.out.println(5^6); // XOR Operator
+        System.out.println(~5); // One Complement
+        System.out.println(~0);
+        System.out.println(5<<2); // Left Shift
+        System.out.println(6>>1); // Right shift
+
+
+    }
+}
