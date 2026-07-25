292dcee448f8fa544945d55580a6d80069165cd3
webdevelopment <abhayvishwakrma102@gmail.com>
2026-07-25 13:30:11 +0530

    even power

--- DSA/Bit_Manipulation/evenPower.java
+++ DSA/Bit_Manipulation/evenPower.java
@@ -1,3 +1,9 @@
 public class evenPower{
-    
+    public static boolean isPowerOfTwo(int n){
+        return(n&(n-1))==0;
+    }
+    public static void main(String arg[]){
+        System.out.println(isPowerOfTwo(8));
+        System.out.println(isPowerOfTwo(7));
+    }
 }
