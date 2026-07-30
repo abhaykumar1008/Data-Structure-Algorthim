034449fd278d9cdebf2a3226657d36644ada662c
webdevelopment <abhayvishwakrma102@gmail.com>
2026-07-30 15:59:56 +0530

    polymorphism

---
+++ DSA/OOPs/Polymorphism.java
@@ -0,0 +1,37 @@
+public class Polymorphism{
+    public static void main(String args[]){
+        /*calculator cal = new calculator();
+        System.out.println(cal.sum(1,2));
+        System.out.println(cal.sum((float)1.5,(float)2.5));
+        System.out.println(cal.sum(1,2,3));*/
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+// method overloading 
+class calculator{
+    int sum(int a , int b){
+        return a+b;
+    }
+    float sum(float a, float b){
+        return a+b;
+    }
+    int sum(int a, int b,int c){
+        return a+b+c;
+    }
+}
+
+
+
+// Method Overriding
+class Animal {
+    void eat(){
+        System.out.println("eats anything");
+    }
+}
+class Deer extends Animal {
+    void eat (){
+        System.out.println("eats grass");
+    }
+}
