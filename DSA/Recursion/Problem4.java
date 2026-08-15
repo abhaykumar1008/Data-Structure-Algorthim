7e93c7aed0cbdda1608b3cba47f4585cfccede94
webdevelopment <abhayvishwakrma102@gmail.com>
2026-08-15 12:12:06 +0530

    problem 4

---
+++ DSA/Recursion/Problem4.java
@@ -0,0 +1,15 @@
+public class Problem4{
+    public static int calcSum(int n){
+        if(n==1){
+            return 1;
+        }
+        int snm1 = calcSum(n-1);
+        int sn = n+snm1;
+        return sn;
+
+    }
+    public static void main(String arg[]){
+        int n = 5;
+        System.out.println(calcSum(n));
+    }
+}
