9c499a8b120336c829cd1a8ec9d6d57fb687a0aa
webdevelopment <abhayvishwakrma102@gmail.com>
2026-08-15 12:51:01 +0530

    problem 6

---
+++ DSA/Recursion/Problem6.java
@@ -0,0 +1,17 @@
+public class Problem6{
+    public static boolean isSorted(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return isSorted(arr,i+1);
+    }
+    public static void main(String arg[]){
+        int arr[] = {1,2,3,4,5};
+        System.out.println(isSorted(arr,0));
+        int arr1[] = {1,2,3,5,4};
+        System.out.println(isSorted(arr1,0));
+    }
+}
