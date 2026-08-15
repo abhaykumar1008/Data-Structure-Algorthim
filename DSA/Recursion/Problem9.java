5e0e7c94a7734d0b32a44ddc0eee0cff752ecdfa
webdevelopment <abhayvishwakrma102@gmail.com>
2026-08-15 14:37:24 +0530

    problem 9

--- DSA/Recursion/Problem9.java
+++ DSA/Recursion/Problem9.java
@@ -1,3 +1,15 @@
 public class Problem9{
-    
+    public static int pow(int x , int n){
+        if(n==0){
+            return 1;
+        }
+        int xnm1 = pow(x,n-1);
+        int xn = x*xnm1;
+        return xn;
+    }
+    public static void main(String arg[]){
+        int x = 2;
+        int n = 10;
+        System.out.println(pow(x,n));
+    }
 }
