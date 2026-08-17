92f52eb7968d436ef73e2e006689648e013a3b10
webdevelopment <abhayvishwakrma102@gmail.com>
2026-08-17 14:12:04 +0530

    recursion part 2 tiling problem

---
+++ DSA/Recursion_part2/TilingPro.java
@@ -0,0 +1,17 @@
+public class TilingPro{
+    public static int tilingProblem(int n){
+        // base case 
+        if(n==0 || n==1){
+            return 1;
+        }
+        // kaam 
+        int fnm1 = tilingProblem(n-1); // vertical choice
+        int fnm2 = tilingProblem(n-2); // horizontal choice
+        int totalways = fnm1 + fnm2;
+        return totalways;
+    }
+
+    public static void main(String args[]){
+        System.out.println(tilingProblem(3));
+    }
+}
